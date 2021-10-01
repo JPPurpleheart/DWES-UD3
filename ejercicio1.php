31a866e1bb0088bf158d1f786dba0629dff76869
jppurpleheart <josepablomachucagonzalez@gmail.com>
2021-10-01 14:10:53 +0200

    ejercicio1.php

---
+++ ejercicio1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $num1 = 4;
+        $num2 = 8;
+        if ($num1 < $num2) {
+            echo "$num2 es el mayor de los dos numeros.";
+        } else
+            echo "$num1 es el mayor de los dos numeros.";
+    ?>
+</body>
+</html>

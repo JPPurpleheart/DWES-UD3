b76e391ad832a215bc60fd5d3658dc65e7de758f
jppurpleheart <josepablo.machuca.gonzalez@gmail.com>
2021-10-13 11:10:04 +0200

    ejercicio8.php

---
+++ ejercicio8.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border = "1">
+        <tr>
+            <th>Elevado a</th>
+            <th>Resultado</th>
+        </tr>
+        <?php
+            $pow = 7;
+            $twopow = 0;
+            for ($i = 0; $i <= $pow; $i ++)
+            {
+                if ($i == 4){
+                    continue;
+                }
+                else if ($i == 6)
+                {
+                    break;
+                }
+                else
+                    $twopow = pow(2, $i);
+                    echo "<tr>", "<td>", $i, "</td>", "<td>", $twopow, "</td>", "</tr>";
+            }
+        ?>
+    </table>
+</body>
+</html>
